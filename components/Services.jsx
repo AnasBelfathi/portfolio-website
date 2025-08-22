7d41a540c62c4b26e6c4868d4a178202093ed20a
Anas <a@gmail.com>
2025-08-22 10:36:26 +0100

    Mise à jour du contenu

--- components/Services.jsx
+++ components/Services.jsx
@@ -5,23 +5,23 @@ import { motion } from "motion/react"
 
 const Services = () => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
+    <motion.div 
+      initial={{ opacity: 0 }} 
+      whileInView={{ opacity: 1 }} 
       transition={{ duration: 1 }}
-      id="services"
+      id="services" 
       className='w-full px-[12%] py-10 scroll-mt-20'
     >
-      <motion.h4
-        initial={{ y: -20, opacity: 0 }}
-        whileInView={{ y: 0, opacity: 1 }}
+      <motion.h4 
+        initial={{ y: -20, opacity: 0 }} 
+        whileInView={{ y: 0, opacity: 1 }} 
         transition={{ delay: 0.3, duration: 0.5 }}
         className='text-center mb-2 text-lg font-Ovo'
       >
         What I offer
       </motion.h4>
 
-      <motion.h2
+      <motion.h2 
         initial={{ y: -20, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ delay: 0.5, duration: 0.5 }}
@@ -30,7 +30,7 @@ const Services = () => {
         Research & Engineering Services
       </motion.h2>
 
-      <motion.p
+      <motion.p 
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ delay: 0.7, duration: 0.5 }}
@@ -45,20 +45,18 @@ const Services = () => {
         transition={{ delay: 0.9, duration: 0.6 }}
         className='grid grid-cols-auto gap-6 my-10'
       >
-        {serviceData.map(({ icon, title, description, link }, index) => (
-          <motion.div
-            whileHover={{ scale: 1.05 }}
+        {serviceData.map(({icon, title, description}, index)=>(
+          <motion.div 
+            whileHover={{scale: 1.05}}
             key={index}
-            className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'
+            className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'
           >
-            <Image src={icon} alt='' className='w-10' />
+            <Image src={icon} alt='' className='w-10'/>
             <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
             <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>
               {description}
             </p>
-            <a href={link} className='flex items-center gap-2 text-sm mt-5'>
-              Read more <Image alt='' src={assets.right_arrow} className='w-4' />
-            </a>
+            {/* Le bouton "Read more" a été supprimé */}
           </motion.div>
         ))}
       </motion.div>
